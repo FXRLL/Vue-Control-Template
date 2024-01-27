66299fa569d178e396dd05bba21a76bc8d072d80
FXRLL <3011286045@qq.com>
2024-01-27 21:10:21 +0800

    finish config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,23 +1,7 @@
 <script setup>
-import { ref } from 'vue'
 
-defineProps({
-  msg: String,
-})
-
-const count = ref(0)
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
+<template></template>
 
-  <div class="card">
-    <el-button type="button" @click="count++">count is {{ count }}</el-button>
-  </div>
-</template>
-
-<style scoped>
-.read-the-docs {
-  color: #888;
-}
-</style>
+<style scoped></style>
